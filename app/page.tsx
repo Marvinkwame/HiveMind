303122b480005edf10b56f28ba9e0f298ced18c2
Marvinkwame <marvinkudjo@gmail.com>
2024-11-05 00:40:56 +0000

    Testing convex mutations and queries

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,39 @@
 
 import Image from "next/image";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
-import { Authenticated, Unauthenticated, useQuery } from "convex/react";
+import {
+  Authenticated,
+  Unauthenticated,
+  useQuery,
+  useMutation,
+} from "convex/react";
 import { api } from "../convex/_generated/api";
 
 export default function Home() {
+  const createDocument = useMutation(api.document.createDocument);
+
+  const getDocuemnts = useQuery(api.document.getDocuments);
+
   return (
-    <>
+    <main className="flex flex-col items-center justify-center min-h-screen">
       <Unauthenticated>
         <SignInButton />
       </Unauthenticated>
       <Authenticated>
         <UserButton />
+        <button
+          onClick={() =>
+            createDocument({
+              title: "Asem na Waka",
+            })
+          }
+        >
+          Click to insert
+        </button>
+        {getDocuemnts?.map((item) => {
+          return <p key={item._id}>{item.title}</p>;
+        })}
       </Authenticated>
-    </>
+    </main>
   );
 }
